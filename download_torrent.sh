7f542c39da97fb0400ed6a92f763c63951ca87d4
MrKristofere <119357545+MrKristofere@users.noreply.github.com>
2024-11-20 09:01:05 +0300

    Update download_torrent.sh

--- download_torrent.sh
+++ download_torrent.sh
@@ -15,7 +15,28 @@ download_torrent_file() {
     fi
 }
 
-# Функция для загрузки через магнет-ссылку
+create_dht_dat() {
+    if ! [ -e $HOME/.cache/aria2/dht.dat ]; then
+    # hide false error: Exception caught while loading DHT routing table
+    # https://github.com/aria2/aria2/issues/1253
+    # based on aria2/src/DHTRoutingTableDeserializer.cc
+    hex=""
+    hex+="a1 a2" # 0+2: magic
+    hex+="02" # 2+1: format
+    hex+="00 00 00" # 3+3
+    hex+="00 03" # 6+2: version
+    hex+=$(printf "%016x\n" $(date --utc +%s)) # 8+8: time
+    hex+="00 00 00 00 00 00 00 00" # 16+8: localnode
+    hex+=$(dd if=/dev/random bs=1 count=40 status=none | sha1sum - | cut -c1-40) # 24+20: localnode ID
+    hex+="00 00 00 00" # 44+4: reserved
+    hex+="00 00 00 00" # 48+4: num_nodes uint32_t
+    hex+="00 00 00 00" # 52+4: reserved
+    # 56 bytes
+    mkdir -p $HOME/.cache/aria2
+    echo $hex | xxd -r -p >$HOME/.cache/aria2/dht.dat
+  fi
+}
+
 download_magnet_link() {
     local magnet_link=$1
     local output_dir=$2
@@ -38,28 +59,6 @@ download_magnet_link() {
         echo "Магнет-ссылка не предоставлена!"
         exit 1
     fi
-}
-
-create_dht_dat() {
-    if ! [ -e $HOME/.cache/aria2/dht.dat ]; then
-    # hide false error: Exception caught while loading DHT routing table
-    # https://github.com/aria2/aria2/issues/1253
-    # based on aria2/src/DHTRoutingTableDeserializer.cc
-    hex=""
-    hex+="a1 a2" # 0+2: magic
-    hex+="02" # 2+1: format
-    hex+="00 00 00" # 3+3
-    hex+="00 03" # 6+2: version
-    hex+=$(printf "%016x\n" $(date --utc +%s)) # 8+8: time
-    hex+="00 00 00 00 00 00 00 00" # 16+8: localnode
-    hex+=$(dd if=/dev/random bs=1 count=40 status=none | sha1sum - | cut -c1-40) # 24+20: localnode ID
-    hex+="00 00 00 00" # 44+4: reserved
-    hex+="00 00 00 00" # 48+4: num_nodes uint32_t
-    hex+="00 00 00 00" # 52+4: reserved
-    # 56 bytes
-    mkdir -p $HOME/.cache/aria2
-    echo $hex | xxd -r -p >$HOME/.cache/aria2/dht.dat
-  fi
 }
 
 # Главная функция
