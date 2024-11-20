05dd25725fb1e6e69e887ef219f2fcdaca178c16
MrKristofere <119357545+MrKristofere@users.noreply.github.com>
2024-11-20 09:51:20 +0300

    Update download_torrent.sh

--- download_torrent.sh
+++ download_torrent.sh
@@ -14,13 +14,13 @@ download_torrent_file() {
     fi
 }
 
-# Функция для загрузки по магнет-ссылке или хешу торрента
-download_torrent() {
-    local torrent_link=$1
+# Функция для загрузки через магнет-ссылку
+download_magnet_link() {
+    local magnet_link=$1
     local output_dir=$2
 
-    if [ -n "$torrent_link" ]; then
-        echo "Начинаем загрузку с ссылки: $torrent_link..."
+    if [ -n "$magnet_link" ]; then
+        echo "Начинаем загрузку с магнет-ссылки: $magnet_link..."
         aria2c \
           -d "$output_dir" \
           --out "$downloaded_file" \
@@ -32,9 +32,24 @@ download_torrent() {
           --dht-entry-point=dht.libtorrent.org:25401 \
           --seed-time=0 \
           --continue=true \
-          "$torrent_link" || { echo "Ошибка загрузки файла!"; exit 1; }
+          "$magnet_link" || { echo "Ошибка загрузки файла!"; exit 1; }
     else
-        echo "Не предоставлена ссылка или хеш для скачивания!"
+        echo "Магнет-ссылка не предоставлена!"
+        exit 1
+    fi
+}
+
+# Функция для загрузки по хешу торрента
+download_torrent_by_hash() {
+    local torrent_hash=$1
+    local output_dir=$2
+
+    if [ -n "$torrent_hash" ]; then
+        echo "Начинаем загрузку с хеша торрента: $torrent_hash..."
+        magnet_link="magnet:?xt=urn:btih:$torrent_hash"  # Генерируем магнет-ссылку
+        download_magnet_link "$magnet_link" "$output_dir"  # Загружаем по магнет-ссылке
+    else
+        echo "Хеш торрента не предоставлен!"
         exit 1
     fi
 }
@@ -56,17 +71,16 @@ main() {
     # Создание директории для загрузок, если она не существует
     mkdir -p "$output_dir"
 
-    # Проверка, является ли переданная ссылка магнет-ссылкой
+    # Проверка, является ли переданная ссылка магнет-ссылкой или файлом
     local downloaded_file=""
     if [[ "$torrent_url" =~ ^magnet: ]]; then
-        download_torrent "$torrent_url" "$output_dir"
+        download_magnet_link "$torrent_url" "$output_dir"
         downloaded_file="$output_dir/downloaded_file"
     elif [[ -f "$torrent_url" ]]; then
         download_torrent_file "$torrent_url" "$output_dir"
         downloaded_file=$(find "$output_dir" -type f | head -n 1)
     elif [[ -n "$torrent_hash" ]]; then
-        magnet_link="magnet:?xt=urn:btih:$torrent_hash"
-        download_torrent "$magnet_link" "$output_dir"
+        download_torrent_by_hash "$torrent_hash" "$output_dir"
         downloaded_file="$output_dir/downloaded_file"
     else
         echo "Неверная ссылка или файл не существует!"
